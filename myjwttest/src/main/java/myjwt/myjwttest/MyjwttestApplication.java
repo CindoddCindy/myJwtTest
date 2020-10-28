039d337ea1d8b2a83241efae153013968316782c
CindoddCindy <cindodcindy@gmail.com>
2020-10-28 19:33:15 +0700

    tambah folder jwt spring

---
+++ myjwttest/src/main/java/myjwt/myjwttest/MyjwttestApplication.java
@@ -0,0 +1,13 @@
+package myjwt.myjwttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjwttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjwttestApplication.class, args);
+	}
+
+}
---
+++ myjwttest/src/test/java/myjwt/myjwttest/MyjwttestApplicationTests.java
@@ -0,0 +1,13 @@
+package myjwt.myjwttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjwttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
