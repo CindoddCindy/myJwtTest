8fa235354a469ef614e8fccd608dfaabc9b2f428
CindoddCindy <cindodcindy@gmail.com>
2020-11-01 17:08:35 +0700

    tambah repository

--- myjwttest/src/main/java/myjwt/myjwttest/repository/RoleRepository.java
+++ myjwttest/src/main/java/myjwt/myjwttest/repository/RoleRepository.java
@@ -1,4 +1,11 @@
 package myjwt.myjwttest.repository;
 
-public class RoleRepository {
+import myjwt.myjwttest.model.Role;
+import myjwt.myjwttest.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
 }
--- myjwttest/src/main/java/myjwt/myjwttest/repository/UserRepository.java
+++ myjwttest/src/main/java/myjwt/myjwttest/repository/UserRepository.java
@@ -1,4 +1,22 @@
 package myjwt.myjwttest.repository;
 
-public class UserRepository {
+import myjwt.myjwttest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findBuUserNameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userId);
+
+    Optional<User> findByUsername (String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
